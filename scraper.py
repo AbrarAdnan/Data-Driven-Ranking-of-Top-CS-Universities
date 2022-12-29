bb0217873c6e59619aed63a14fae1596b379548d
Abrar Adnan <abrar.adnan01@gmail.com>
2022-12-29 23:02:17 +0600

    Updated requirements and Scraper

--- scraper.py
+++ scraper.py
@@ -1,14 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.select import Select
 import time
 import pandas as pd
 import pycountry
 import os
 import argparse
-
-
-
 
 
 columns = ['Rank', 'University Name', 'Country', 'Count', 'Faculty']
@@ -64,13 +60,16 @@ def main():
     #ADBLOCK
     sponsor = driver.find_element(By.XPATH, '//*[@id="sponsor"]/button[2]')
     if sponsor.is_displayed():
-        print("Blocked a popup Ad")
+        print("==========Blocked a popup Ad===========")
         sponsor.click()
+        time.sleep(3)
     # Get table of a region
     region = driver.find_element(By.XPATH, '//*[@id="regions"]')
     options = region.find_elements(By.TAG_NAME, "option")
     for option in options:
         if option.text == "the world":
+            print("==========The World=============")
+            time.sleep(3)
             option.click()
 
     # Set the timeline
@@ -78,36 +77,47 @@ def main():
     years = timeline.find_elements(By.TAG_NAME, "option")
     for year in years:
         if year.text == "2000":
+            print("==========2000=============")
+            time.sleep(3)
             year.click()
     # Deselect all topics of focus
     all_btn = driver.find_element(By.XPATH, "//*[@id='all_areas_off']")
     all_btn.click()
+    time.sleep(5)
 
     # Select the specific subjects
     arch = driver.find_element(By.XPATH,'//*[@id="arch"]')
     arch.click()
+    time.sleep(1)
     compnet = driver.find_element(By.XPATH,'//*[@id="comm"]')
     compnet.click()
+    time.sleep(1)
     sec = driver.find_element(By.XPATH,'//*[@id="sec"]')
     sec.click()
+    time.sleep(1)
     ops = driver.find_element(By.XPATH,'//*[@id="ops"]')
     ops.click()
+    time.sleep(1)
     proglang = driver.find_element(By.XPATH,'//*[@id="plan"]')
     proglang.click()
+    time.sleep(1)
     soft = driver.find_element(By.XPATH,'//*[@id="soft"]')
     soft.click()
-    print("selected subjects")
+    time.sleep(1)
+    print("===========Selected subjects=========")
 
     # Scroll the table to load it fully
     scrollbar = driver.find_element(By.XPATH, '//*[@id="success"]/div')
     driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", scrollbar)
-    print("Scroll")
+    print("==========Scroll===========")
+    time.sleep(3)
 
     # Scrap data from the table into a variable and clean it
     ranking_table = driver.find_element(By.XPATH, "/html/body/div[5]/form/div/div[2]/div[2]/div/div/table/tbody")
     driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", ranking_table)
     rows = ranking_table.find_elements(By.TAG_NAME, "tr")
-    
+    time.sleep(3)
+
     for elements in rows:
         if elements.text:
             
@@ -117,7 +127,7 @@ def main():
             uni_list.append(cleanup(elements.text, country_name))
 
     df = pd.DataFrame(uni_list,columns=columns)
-    df.to_csv("best_uni.csv")
+    df.to_csv("best_uni_list.csv")
 
 if __name__ == "__main__":
     main()
