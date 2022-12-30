faf72f43c40f39b6ab997b6379cdba9685925ab8
Abrar Adnan <abrar.adnan01@gmail.com>
2022-12-30 21:40:07 +0600

    optimized code

--- scraper.py
+++ scraper.py
@@ -5,29 +5,36 @@ import pandas as pd
 import pycountry
 import os
 import argparse
+import sys
 
 
 columns = ['Rank', 'University Name', 'Country', 'Count', 'Faculty']
 uni_list = []
 
-def cleanup(s,country):
-    # Split the string into a list of words
-    words = s.split()
+def cleanup(uni_string,country):
+    # Cleans up the string with the country name, university name, rank
+    # Count and Faculty
 
+    # Split the string into a list of words
+    words = uni_string.split()
+    
+    # Split the words into a their respective parts
     rank = words[0]
     count = words[-2]
     faculty = words[-1]
     
     middle_str = words[1:-2]
+    # remove the "►" from the middle string
     middle_str = [word for word in middle_str if word != "►"]
 
-    # Join the remaining words into a single string
+    # Join the remaining words into a single string for uni name
     uni_name = " ".join(middle_str)
-
 
     return (rank, uni_name, country, count, faculty)
 
 def getCountry(c):
+    # Extract the country name from the src attribute of an img element.
+
     img = c.find_element(By.TAG_NAME,"img")
     src = img.get_attribute("src")
         
@@ -36,103 +43,161 @@ def getCountry(c):
         
     # Split the file name into the country code and the extension
     country_code, extension = os.path.splitext(file_name)
-    if country_code =='uk':# Fixing a problem to recognize England
+
+    # Fixing a problem to recognize England
+    if country_code =='uk':
         country_code = 'GB'
+
+    # Look up the country name using the country code
     country_name = pycountry.countries.get(alpha_2=country_code)
+
     return country_name.name
 
 
 def main():
     start_time = time.time()
-    parser = argparse.ArgumentParser(description="Please specify a chromedriver path. For more information please refer to the documentation here https://github.com/AbrarAdnan/Week-6-Project/blob/master/README.md#how-to-build-the-source-code-and-run-it")
-    parser.add_argument('--chromedriver_path', type=str, required=True)
+    # Create the parser
+    #parser = argparse.ArgumentParser(description="Specify a chromedriver path. For more information, see the documentation at https://github.com/AbrarAdnan/Week-6-Project/blob/master/README.md#how-to-build-the-source-code-and-run-it")
+    parser = argparse.ArgumentParser()
+    # Add the chromedriver path argument
+    parser.add_argument("--chromedriver_path", type=str, help="Path to the chromedriver executable")
+
+    # Parse the command-line arguments
     args = parser.parse_args()
 
-    #if args.chromedriver_path is None:
-    #    parser.error("Please specify a chromedriver path. For more information please refer to the documentation here https://github.com/AbrarAdnan/Week-6-Project/blob/master/README.md#how-to-build-the-source-code-and-run-it")
-
+    # Get the chromedriver path
     webdriver_path = args.chromedriver_path
     
+    try:
+        # Create a Chrome webdriver
+        driver = webdriver.Chrome(webdriver_path)
 
-    driver = webdriver.Chrome(webdriver_path)
-    url = f"https://csrankings.org/#/index?all&world"
-    driver.get(url)
+        # Set the url to visit
+        url = f"https://csrankings.org/#/index?all&world"
 
-    #ADBLOCK
-    sponsor = driver.find_element(By.XPATH, '//*[@id="sponsor"]/button[2]')
-    if sponsor.is_displayed():
-        print("==========Blocked a popup Ad==========")
+        # Visit the url
+        driver.get(url)
+    
+    except Exception as e:
+        # Print an error message and exit if an exception occurs
+        print("An error occurred with chromedriver, Please run the script again", e)
+        sys.exit(1)
+
+    # Block the sponsor popup if it appears randomly
+    try:
+        # Find the sponsor button
+        sponsor = driver.find_element(By.XPATH, '//*[@id="sponsor"]/button[2]')
+        # Click on the sponsor button
         sponsor.click()
+    except:
+        # If the sponsor button is not found or not displayed, do nothing
+        pass
+
+
+    try:
+        # Find the dropdown menu for selecting the year range
+        timeline =  driver.find_element(By.XPATH, '//*[@id="fromyear"]')
+        # Find the options in the dropdown menu
+        years = timeline.find_elements(By.TAG_NAME, "option")
+
+        # Select the "2000" option
+        for year in years:
+            if year.text == "2000":
+                print(">>>Selected year: 2000")
+                time.sleep(3)
+                year.click()
+        # Find the "deselect all" button for the topic of focus
+        all_button = driver.find_element(By.XPATH, "//*[@id='all_areas_off']")
+        # Click the button to deselect all topics
+        all_button.click()
+        time.sleep(5)
+    except Exception as e:
+        # Print an error message if an exception occurs
+        print("An error occurred while selecting the year range and deselecting all topics:", e)
+        sys.exit(1)
+
+    try:
+    # Find the checkboxes for each subject
+        arch_checkbox = driver.find_element(By.XPATH,'//*[@id="arch"]')
+        compnet_checkbox = driver.find_element(By.XPATH,'//*[@id="comm"]')
+        sec_checkbox = driver.find_element(By.XPATH,'//*[@id="sec"]')
+        ops_checkbox = driver.find_element(By.XPATH,'//*[@id="ops"]')
+        proglang_checkbox = driver.find_element(By.XPATH,'//*[@id="plan"]')
+        soft_checkbox = driver.find_element(By.XPATH,'//*[@id="soft"]')
+        
+        # Select the checkboxes
+        checkboxes = [arch_checkbox, compnet_checkbox, sec_checkbox, ops_checkbox, proglang_checkbox, soft_checkbox]
+        for checkbox in checkboxes:
+            checkbox.click()
+            time.sleep(1)
+        
+        print(">>>Selected subjects")
+
+    except Exception as e:
+        # Print an error message if an exception occurs
+        print("An error occurred while selecting the subjects:", e)
+        sys.exit(1)
+
+    try:
+        # Find the scrollbar element
+        scrollbar = driver.find_element(By.XPATH, '//*[@id="success"]/div')
+        
+        # Scroll to the bottom of the element
+        driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", scrollbar)
+        print(">>>Loaded all the university in the table")
         time.sleep(3)
-    # Get table of a region
-    region = driver.find_element(By.XPATH, '//*[@id="regions"]')
-    options = region.find_elements(By.TAG_NAME, "option")
-    for option in options:
-        if option.text == "the world":
-            print("==========Selected region: The World==========")
-            time.sleep(3)
-            option.click()
 
-    # Set the timeline
-    timeline =  driver.find_element(By.XPATH, '//*[@id="fromyear"]')
-    years = timeline.find_elements(By.TAG_NAME, "option")
-    for year in years:
-        if year.text == "2000":
-            print("==========Selected year: 2000==========")
-            time.sleep(3)
-            year.click()
-    # Deselect all topics of focus
-    all_btn = driver.find_element(By.XPATH, "//*[@id='all_areas_off']")
-    all_btn.click()
-    time.sleep(5)
+    except Exception as e:
+        # Print an error message if an exception occurs
+        print("An error occurred while scrolling the table:", e)
+        sys.exit(1)
 
-    # Select the specific subjects
-    arch = driver.find_element(By.XPATH,'//*[@id="arch"]')
-    arch.click()
-    time.sleep(1)
-    compnet = driver.find_element(By.XPATH,'//*[@id="comm"]')
-    compnet.click()
-    time.sleep(1)
-    sec = driver.find_element(By.XPATH,'//*[@id="sec"]')
-    sec.click()
-    time.sleep(1)
-    ops = driver.find_element(By.XPATH,'//*[@id="ops"]')
-    ops.click()
-    time.sleep(1)
-    proglang = driver.find_element(By.XPATH,'//*[@id="plan"]')
-    proglang.click()
-    time.sleep(1)
-    soft = driver.find_element(By.XPATH,'//*[@id="soft"]')
-    soft.click()
-    time.sleep(1)
-    print("==========Selected subjects==========")
+    try:
+        # Find the table element
+        ranking_table = driver.find_element(By.XPATH, "/html/body/div[5]/form/div/div[2]/div[2]/div/div/table/tbody")
+        
+        # Scroll to the bottom of the table
+        driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", ranking_table)
+        time.sleep(3)
+        
+        # Find all the rows in the table
+        rows = ranking_table.find_elements(By.TAG_NAME, "tr")
+        
+        print(">>>Raw data of universities found will be shown in real time below")
+        # Iterate through the rows and get the country name for each row
+        tick = 0
+        for row in rows:
+            if row.text:
+                tick = tick + 1
+                
+                print(row.text)
+                country_name = getCountry(row)
+                uni_list.append(cleanup(row.text, country_name))
+                if tick == 10:
+                    break
 
-    # Scroll the table to load it fully
-    scrollbar = driver.find_element(By.XPATH, '//*[@id="success"]/div')
-    driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", scrollbar)
-    print("==========Loaded all the university in the table==========")
-    time.sleep(3)
+    except Exception as e:
+        # Print an error message if an exception occurs
+        print("An error occurred while scraping data from the table:", e)
+        sys.exit(1)
 
-    # Scrap data from the table into a variable and clean it
-    ranking_table = driver.find_element(By.XPATH, "/html/body/div[5]/form/div/div[2]/div[2]/div/div/table/tbody")
-    driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", ranking_table)
-    rows = ranking_table.find_elements(By.TAG_NAME, "tr")
-    time.sleep(3)
+    try:
+        # Create a DataFrame from the list of university data
+        df = pd.DataFrame(uni_list, columns=columns)
+        
+        # Save the DataFrame to a CSV file
+        df.to_csv("best_uni_list.csv")
+        print('Sucess: Saved the output data in the best_uni_list.csv file')
+        
+        # Calculate the total time taken to run the script
+        end_time = time.time()
+        time_taken = end_time - start_time
+        print(f"Time taken: {time_taken / 60} minutes")
 
-    for elements in rows:
-        if elements.text:
-            
-            country_name = getCountry(elements)
-            print(elements.text)
-            print(country_name)
-            uni_list.append(cleanup(elements.text, country_name))
-
-    df = pd.DataFrame(uni_list,columns=columns)
-    df.to_csv("best_uni_list.csv")
-    print('Sucess: Saved the output data in the best_uni_list.csv file')
-    end_time = time.time()
-    time_taken = end_time-start_time
-    print(f"Time taken: {time_taken / 60} minutes")
+    except Exception as e:
+        # Print an error message if an exception occurs
+        print("An error occurred while saving the output data:", e)
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
